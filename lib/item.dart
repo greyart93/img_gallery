a43d1856c984343222e30b3d17aba2db7d05ddf4
Seuss <seussstaudinger@gmail.com>
2024-04-18 23:52:59 +0000

    item dart file

---
+++ lib/item.dart
@@ -0,0 +1,41 @@
+List<Item> items = [
+  Item(
+    "https://img.freepik.com/free-photo/rise-humanoids-with-advanced-headgear-generative-ai_8829-2877.jpg",
+    "The rise of humanoids",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/cat-warriors-with-burning-eyes-generative-ai_8829-2909.jpg",
+    "Cat warriors",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/majestic-grand-piano-with-red-lights-generative-ai_188544-7789.jpg",
+    "Majestic grand piano",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/staircase-that-led-light-was-like-bridge-worlds-generative-ai_8829-2919.jpg",
+    "Road to light",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/refreshing-citrus-cocktail-garnished-with-juicy-fruit-slices-generative-ai_188544-12394.jpg",
+    "Refreshing cocktail",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/glass-red-wine-cups-close-up-generative-ai_188544-12239.jpg",
+    "Glass of wine",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/beauty-product-exploding-with-vibrant-colors-powder-generative-ai_188544-9687.jpg",
+    "Еxplosion of colors",
+  ),
+  Item(
+    "https://img.freepik.com/free-photo/abstract-glowing-flame-drops-electric-illumination-generative-ai_188544-8092.jpg",
+    "Flame drops",
+  ),
+];
+
+class Item {
+  final String imageUrl;
+  final String name;
+
+  Item(this.imageUrl, this.name);
+}
